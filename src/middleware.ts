bc6cb355159e7aafd7cc0002d6719e265b90ae74
unknown <facuugerez37@gmail.com>
2024-03-11 12:58:54 -0300

    auth middleware fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
-
+export default authMiddleware({
+  publicRoutes: ["/api/:path*"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
